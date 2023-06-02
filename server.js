b1930e0301515b9c3990bac28b946f8bfad06a55
Jose <josemonagas212@gmail.com>
2023-06-02 19:21:02 -0400

    create magic 8 ball route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // Routes
 
-// Define a "root" route directly on app
+// Greeting Route
 app.get("/", (req, res) => {
   res.send("<h1>Hello There</h1>");
 });
@@ -17,11 +17,43 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`<h1>Hey there, ${name}</h1>`);
 });
 
+// Tip Calculator
 app.get("/tip/:total/:tipPercentage", (req, res) => {
   const total = parseFloat(req.params.total);
   const tipPercentage = parseFloat(req.params.tipPercentage);
   const tip = (total * tipPercentage) / 100;
   res.send(`<h1>${tip}</h1>`);
+});
+
+// Magic 8 Ball
+app.get("/magic/:question", (req, res) => {
+  const question = req.params.question.replace(/%20/g, " ");
+  const responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  const magic8BallResponse =
+    responses[Math.floor(Math.random() * responses.length)];
+  const htm = `<h1>${question}?</h1><h1>mmmm...</h1><h1>${magic8BallResponse}</h1>`;
+  res.send(htm);
 });
 
 // Tell the server to listen for request
